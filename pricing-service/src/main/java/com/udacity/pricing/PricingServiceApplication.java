4bec9a601cdd70f56753acd8ea6324a14f14754f
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-09-29 20:15:09 +0100

    Enabled Dicovery Client

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -3,10 +3,12 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class PricingServiceApplication {
 
 	public static void main(String[] args) {
