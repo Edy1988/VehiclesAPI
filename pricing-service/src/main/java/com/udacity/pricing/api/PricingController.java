f9dac1aa8b7043294e71a0f7eaeee75cb3027451
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-10-03 13:57:24 +0100

    Removed comments

--- pricing-service/src/main/java/com/udacity/pricing/api/PricingController.java
+++ pricing-service/src/main/java/com/udacity/pricing/api/PricingController.java
@@ -12,18 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-/**
- * Implements a REST-based controller for the pricing service.
- */
 @RestController
 @RequestMapping("/services/price")
 public class PricingController {
 
-    /**
-     * Gets the price for a requested vehicle.
-     * @param vehicleId ID number of the vehicle for which the price is requested
-     * @return price of the vehicle, or error that it was not found.
-     */
     @GetMapping
     public Price get(@RequestParam Long vehicleId) {
         try {
